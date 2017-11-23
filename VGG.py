89091b7d3a7e10ab1424bcfaca9b09605a7a568a
seonghwan <shhong407@naver.com>
2017-11-23 23:45:21 +0900

    VGG 만들고 모델 데이터 받는 부분

---
+++ VGG.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+
+        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+
+        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+
+        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
+
+        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 1),
+        )
+
+    def forward(self, X):
+        h = self.conv1_1(X)
+        h = F.relu(h)
+        h = self.bn1(x)
+        # -----------------------------------------
+        h = self.conv1_2(h)
+        h = F.relu(h)
+        h = self.bn1(x)
+        h = F.max_pool2d(h, kernel_size=(2, 2), stride=(2, 2))
+
+        h = self.conv2_1(h)
+        h = F.relu(h)
+        h = self.bn2(x)
+        # -----------------------------------------
+        h = self.conv2_2(h)
+        h = F.relu(h)
+        h = self.bn2(x)
+        h = F.max_pool2d(h, kernel_size=(2, 2), stride=(2, 2))
+
+        h = self.conv3_1(h)
+        h = F.relu(h)
+        h = self.bn3(x)
+        # -----------------------------------------
+        h = self.conv3_2(h)
+        h = F.relu(h)
+        h = self.bn3(x)
+        h = F.max_pool2d(h, kernel_size=(2, 2), stride=(2, 2))
+
+        h = self.conv4_1(h)
+        h = F.relu(h)
+        h = self.bn4(x)
+        # -----------------------------------------
+        h = self.conv4_2(h)
+        h = F.relu(h)
+        h = self.bn4(x)
+        h = F.max_pool2d(h, kernel_size=(2, 2), stride=(2, 2))
+
+        h = self.conv5_1(h)
+        h = F.relu(h)
+        h = self.bn4(x)
+        # -----------------------------------------
+        h = self.conv5_2(h)
+        h = F.relu(h)
+        h = self.bn4(x)
+        h = F.max_pool2d(h, kernel_size=(2, 2), stride=(2, 2))
+        # ---------------------------------------------------------------
+        # ---------------------------------------------------------------
+        # ---------------------------------------------------------------
+        h = x.view(h.size(0), -1)
+        h = self.classifier(h)
+        return h
